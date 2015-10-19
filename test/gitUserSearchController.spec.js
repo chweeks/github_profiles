e4a2f5d6b52c182722526681126e4f379711cc66
chweeks <chrisharryweeks@gmail.com>
2015-10-19 18:31:15 +0100

    added github api

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -1,18 +1,28 @@
 describe('GitUserSearchController', function() {
   beforeEach(module('GitUserSearch'));
 
-  it('initialises with an empty search result and term', function() {
-    expect(ctrl.searchResult).toBeUndefined();
-    expect(ctrl.searchTerm).toBeUndefined();
-  });
-
   var ctrl;
 
   beforeEach(inject(function($controller) {
     ctrl = $controller('GitUserSearchController');
   }));
 
+  it('initialises with an empty search result and term', function() {
+    expect(ctrl.searchResult).toBeUndefined();
+    expect(ctrl.searchTerm).toBeUndefined();
+  });
+
   describe('when searching for a user', function() {
+
+    var httpBackend;
+      beforeEach(inject(function($httpBackend) {
+        httpBackend = $httpBackend
+        httpBackend
+          .when("GET", "https://api.github.com/search/users?q=hello")
+          .respond(
+            { items: items }
+          );
+      }));
 
     var items = [
       {
@@ -30,6 +40,7 @@ describe('GitUserSearchController', function() {
     it('displays search results', function() {
       ctrl.searchTerm = 'hello';
       ctrl.doSearch();
+      httpBackend.flush();
       expect(ctrl.searchResult.items).toEqual(items);
     });
   });
